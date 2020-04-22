9863e6f63588ed3dc9007b768af9e8d34ec4dfaf
morenoloza98 <morenolozalex98@gmail.com>
2020-04-21 23:46:55 -0500

    Fixed import error in app module

--- EntregaDos/src/app/app.module.ts
+++ EntregaDos/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './mi-components/header/header.component';
+import { HeaderComponent } from './main-components/header/header.component';
 import { MenuComponent } from './main-components/menu/menu.component';
 
 @NgModule({
