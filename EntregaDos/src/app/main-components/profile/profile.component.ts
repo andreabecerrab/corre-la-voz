537e7705e2dd2d160dbb535f32e6ab716eab9235
morenoloza98 <morenolozalex98@gmail.com>
2020-04-22 03:55:06 -0500

    Added variables to components

--- EntregaDos/src/app/main-components/profile/profile.component.ts
+++ EntregaDos/src/app/main-components/profile/profile.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public idUsuario:number;
+  public nombreUsuario:string;
+  public apellidoU:string;
+  public emailU:string;
+  
+  constructor() { 
+    this.idUsuario = 5290;
+    this.nombreUsuario = "Andrea";
+    this.apellidoU = "Becerra";
+    this.emailU = "andrea@gmail.com"
+  }
 
   ngOnInit(): void {
   }
