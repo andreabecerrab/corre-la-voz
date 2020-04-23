f8ecc95b055214e55e455b93caa2ec769c98ee8f
liaDiaz <46771468+liaDiaz@users.noreply.github.com>
2020-04-22 22:49:59 -0500

    merge

--- EntregaDos/src/app/app.module.ts
+++ EntregaDos/src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './main-components/header/header.component';
 import { MenuComponent } from './main-components/menu/menu.component';
 import { AdminDashboardComponent } from './modules/admin/components/admin-dashboard/admin-dashboard.component';
-import { TableStrikesComponent } from './modules/admin/components/table-strikes/table-strikes.component'
+import { TableStrikesComponent } from './modules/admin/components/table-strikes/table-strikes.component';
 import { SidebarComponent } from './main-components/sidebar/sidebar.component';
-import { ProfileComponent } from './main-components/profile/profile.component'
+import { ProfileComponent } from './main-components/profile/profile.component';
 
 import { LandingpageModule } from './modules/landingpage/landingpage.module';
 import { LoginComponent } from './main-components/login/login.component';
