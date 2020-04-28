340ee523846c8cf54759fb72594da5d202401d0c
andreabecerrab <andii1wiki@gmail.com>
2020-04-28 00:32:47 -0500

    sidebar routing

--- EntregaDos/src/app/app.component.ts
+++ EntregaDos/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EntregaDos';
-  userlogin: boolean = true;
+  userlogin: boolean = false;
+  adminlogin: boolean = true;
 }
--- EntregaDos/src/app/modules/admin/admin-routing.module.ts
+++ EntregaDos/src/app/modules/admin/admin-routing.module.ts
@@ -4,12 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { TableStrikesComponent } from './components/table-strikes/table-strikes.component';
 import { ProfileComponent } from '../../main-components/profile/profile.component';
+import { EditProfileComponent } from 'src/app/main-components/edit-profile/edit-profile.component';
 
 const routes: Routes = [
-  { path: 'inicioo', component: AdminDashboardComponent },
+  { path: 'inicio', component: AdminDashboardComponent },
   { path: 'marchas', component: TableStrikesComponent },
   { path: 'perfil', component: ProfileComponent },
-  { path: 'editar-perfil', component: ProfileComponent },
+  { path: 'editar-perfil', component: EditProfileComponent },
 ];
 
 @NgModule({
