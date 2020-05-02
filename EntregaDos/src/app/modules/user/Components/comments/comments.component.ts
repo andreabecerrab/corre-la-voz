0ce78b70c03b41ea6813b7213add9bec40b64af4
liaDiaz <46771468+liaDiaz@users.noreply.github.com>
2020-05-02 01:25:56 -0500

    merge

--- EntregaDos/src/app/modules/user/Components/comments/comments.component.ts
+++ EntregaDos/src/app/modules/user/Components/comments/comments.component.ts
@@ -9,25 +9,30 @@ export class CommentsComponent implements OnInit {
   public fecha: string;
   public nombre: string;
   public handle: string;
-  public contenido: string;
+ 
   public mostrardatos:boolean;
-  public comentario;
+  public comentario="";
+  public listas=[];
+
   
 
   constructor() { 
     this.fecha="20/58/2020";
     this.nombre="Martia Diaz Garcia";
     this.handle="@MartinoMont";
-    this.contenido=" hola";
+   
+    
     
 
   }
 
+
   ngOnInit(): void {
   }
-  onClickMe(){
-    this.mostrardatos=true;
-  //  `` this.comentario=al;``
+  post(){
+   this.listas.push(this.comentario);
+   this.comentario="";
+   this.mostrardatos=true;
 
   }
 
