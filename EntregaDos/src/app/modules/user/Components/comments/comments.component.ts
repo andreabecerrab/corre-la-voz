6ab1f476b764b56239bb7b72934d05e14928f61a
andreabecerrab <andii1wiki@gmail.com>
2020-05-03 01:21:46 -0500

    comments.ts []

--- EntregaDos/src/app/modules/user/Components/comments/comments.component.ts
+++ EntregaDos/src/app/modules/user/Components/comments/comments.component.ts
@@ -7,47 +7,16 @@ import { Comentario } from 'src/app/models/Comentario';
   templateUrl: './comments.component.html',
 })
 export class CommentsComponent implements OnInit {
-<<<<<<< HEAD
-  public fecha: string;
-  public nombre: string;
-  public handle: string;
- 
-  public mostrardatos:boolean;
-  public comentario="";
-  public listas=[];
-
-  
-
-  constructor() { 
-    this.fecha="20/58/2020";
-    this.nombre="Martia Diaz Garcia";
-    this.handle="@MartinoMont";
-   
-    
-    
-
-  }
-
-
-=======
   public comentario: '';
   public mostrados: boolean;
   @Input() comentarios: Comentario[];
 
   constructor(private _marchasServices: MarchaServiceService) {}
->>>>>>> forms
   ngOnInit(): void {
     if (this.comentarios.length > 0) {
       this.mostrados = true;
     }
   }
-<<<<<<< HEAD
-  post(){
-   this.listas.push(this.comentario);
-   this.comentario="";
-   this.mostrardatos=true;
-
-=======
   post() {
     console.log('agregado');
     this._marchasServices.postComentario(2, {
@@ -57,6 +26,5 @@ export class CommentsComponent implements OnInit {
     });
     this.comentario = '';
     this.mostrados = true;
->>>>>>> forms
   }
 }
