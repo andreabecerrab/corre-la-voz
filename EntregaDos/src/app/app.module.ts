001971fdea2f876a73a6f1e4ef850c68aacf81e1
morenoloza98 <morenolozalex98@gmail.com>
2020-05-05 19:04:38 -0500

    Graph included in info tab

--- EntregaDos/src/app/app.module.ts
+++ EntregaDos/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 //main components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './main-components/header/header.component';
@@ -45,6 +46,7 @@ import { TweetsService } from './services/tweets.service';
     UserModule,
     AdminModule,
     AppRoutingModule,
+    ChartsModule
   ],
   exports: [ProfileComponent],
   providers: [MarchaServiceService, TweetsService],
