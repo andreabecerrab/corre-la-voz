772490c08dfdef172848503d0c81ddb2533993a8
andreabecerrab <andii1wiki@gmail.com>
2020-06-08 17:17:37 -0500

    admin guard

--- EntregaDos/src/app/guards/admin-auth.guard.ts
+++ EntregaDos/src/app/guards/admin-auth.guard.ts
@@ -14,15 +14,14 @@ import { AuthenticationService } from '../services/authentication.service';
   providedIn: 'root',
 })
 export class AdminAuthGuard implements CanActivate {
-<<<<<<< HEAD
-  private usuario;
+  private usuarioTipo;
   constructor(
     private userService: UsuarioServiceService,
-    private router: Router
+    private router: Router,
+    private auth: AuthenticationService
   ) {
-    this.usuario = this.userService
-      .getUserUpdatedListener()
-      .subscribe((user) => (this.usuario = user));
+    this.userService.getUsuario();
+    console.log(this.usuarioTipo);
   }
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -32,31 +31,17 @@ export class AdminAuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    if (this.usuario.tipo === 'admin') {
-      return true;
-    } else if (this.usuario.tipo === 'usuario') {
-      this.router.navigate(['/usuario/inicio']);
-      return false;
-    }
-=======
-  private usuarioTipo;
-  constructor(private userService:UsuarioServiceService, private router: Router, private auth: AuthenticationService) {
-    this.userService.getUsuario();
-    console.log(this.usuarioTipo);
-  }
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      this.usuarioTipo = this.userService.getUserUpdatedListener().subscribe((data) => { 
+    this.usuarioTipo = this.userService
+      .getUserUpdatedListener()
+      .subscribe((data) => {
         this.usuarioTipo = data.tipo;
-        if(this.usuarioTipo !== 'admin'){
+        if (this.usuarioTipo !== 'admin') {
           this.router.navigate(['/perfil']);
           return false;
         } else {
           return true;
         }
       });
-      return true;
->>>>>>> 858d93f8180df8a2be5cd4fa290a8a5d099d9c14
+    return true;
   }
 }
